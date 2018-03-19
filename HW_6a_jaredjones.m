54470307f1bc28f128f3cf2852bbd88c50c5afb7
Mech209 <deraj209@yahoo.com>
2018-03-19 03:42:59 -0500

    finalupdate

--- HW_6a_jaredjones.m
+++ HW_6a_jaredjones.m
@@ -5,49 +5,60 @@ N = 10;
 L = pi();
 h = L/(N);
 M = 1;
-A_d = eye(N-1)*(-4*N);
-A_d = A_d + diag(ones(N-2,1),1)*N;
-A_d = A_d + diag(ones(N-2,1),-1)*N;
-Aoff = eye(N-1)*N;
-A = sparse((N-1)*(N-1), (N-1)*(N-1));
-for i = 1:N-1
-    A((i-1)*(N-1)+1:(i-1)*(N-1)+(N-1), (i-1)*(N-1)+1:(i-1)*(N-1)+(N-1)) = A_d;
+%Boundary Conditions
+uo = 0;
+uL = 0;
+vo = 0;
+vL = 0;
+
+A_d = eye(N)*(-4);
+A_d = A_d + diag(ones(N-1,1),1);
+A_d = A_d + diag(ones(N-1,1),-1);
+Aoff = eye(N);
+A = sparse((N)*(N), (N)*(N));
+for i = 1:N
+    A((i-1)*(N)+1:(i-1)*(N)+(N), (i-1)*(N)+1:(i-1)*(N)+(N)) = A_d;
 end
-for i = 2:N-1
-   A((i-2)*(N-1)+1:(i-2)*(N-1)+(N-1), (i-1)*(N-1) +1:(i-1)*(N-1)+(N-1)) = Aoff;
-   A((i-1)*(N-1) +1:(i-1)*(N-1)+(N-1), (i-2)*(N-1)+1:(i-2)*(N-1)+(N-1)) = Aoff;
+for i = 2:N
+   A((i-2)*(N)+1:(i-2)*(N)+(N), (i-1)*(N)+1:(i-1)*(N)+(N)) = Aoff;
+   A((i-1)*(N) +1:(i-1)*(N)+(N), (i-2)*(N)+1:(i-2)*(N)+(N)) = Aoff;
 end
 f = zeros();
-for i = 1:N-1
+for i = 1:N
     x = i*h;
-    for j = 1:N-1
+    for j = 1:N
         y = j*h;
 f(i,j) = -2*M*sin(M*x)*cosh(M*y);
     end 
 end
-f_new = reshape(f, [81,1]);
+
+f_new = reshape(f, [N^2,1]);
+f_new = f_new*h^2;
 u = A\f_new; 
-u = reshape(u, 9,9);
+u = reshape(u, N,N);
 
 %Exact
 u_exact = zeros();
-for i = 1:N-1
+for i = 1:N
     x = i*h;
-    for j = 1:N-1
+    for j = 1:N
         y = j*h;
       u_exact(i,j) = (L-y)*sin(M*x)*sinh(M*y);
     end
 end
 
 %L1 error
-err = u_exact - u;
+err = abs((u_exact - u))./abs(u_exact);
 
 %Plot u at a few y values
-hold on
-plot()
-plot(u,L/2)
-plot(u,3*L/4)
-%Plot u at a few x values
-plot(L/4,u)
-plot(L/2,u)
-plot(3*L/4, u)
+x= zeros();
+for i = 1:N
+    x(i) = i*h;
+for j = 1:N
+    y(j) = j*h;
+end
+end
+hold
+plot(x, u(:,round([L/4 L/2 3*L/4])), x, u_exact(:,round([L/4 L/2 3*L/4])))
+figure
+plot(y, u(round([L/4 L/2 3*L/4]),:), y, u_exact(round([L/4 L/2 3*L/4]),:))
